0e9d00c2c0c66f3f2364a83e3f2fb3ebec40fcf3
JuliaNakano <jumartinsnakano@gmail.com>
2021-09-07 19:16:19 -0300

    Solução do exercício Músia

---
+++ Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace exercicio_musica_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Console.ForegroundColor= ConsoleColor.Green;
+                Console.WriteLine("Believer");
+                Console.ResetColor();
+                Console.WriteLine("(Imagine Dragons)");
+                Console.WriteLine("");
+
+                Console.WriteLine("First things first");
+                Console.WriteLine("I'ma say all the words inside my head");
+                Console.WriteLine("I'm fired up and tired of the way");
+                Console.WriteLine("That things have been, oh-ooh");
+                Console.WriteLine("The way that things have been, oh-ooh");
+                Console.WriteLine("Second things second");
+                Console.WriteLine("Don't you tell me what you think that I could be");
+                Console.WriteLine("I'm the one at the sail, I'm the master of my sea, oh-ooh");
+                Console.WriteLine("The master of my sea, oh-ooh");
+                Console.WriteLine("");
+
+                Console.WriteLine("I was broken from a young age");
+                Console.WriteLine("Taking my sulking to the masses");
+                Console.WriteLine("Writing my poems for the few");
+                Console.WriteLine("That look at me, took to me, shook to me, feeling me");
+                Console.WriteLine("Singing from heartache from the pain");
+                Console.WriteLine("Taking my message from the veins");
+                Console.WriteLine("Speaking my lesson from the brain");
+                Console.WriteLine("Seeing the beauty through the");
+                Console.WriteLine("");
+
+                Console.ForegroundColor= ConsoleColor.DarkBlue;
+                Console.WriteLine("Pain!");
+                Console.WriteLine("You made me a, you made me a believer, believer");
+                Console.WriteLine("Pain!");
+                Console.WriteLine("You break me down and build me up, believer, believer");
+                Console.WriteLine("Pain!");
+                Console.WriteLine("Oh, let the bullets fly, oh, let them rain");
+                Console.WriteLine("My life, my love, my drive, it came from");
+                Console.WriteLine("Pain!");
+                Console.WriteLine("You made me a, you made me a believer, believer");
+                Console.ResetColor();
+                Console.WriteLine("");
+
+                Console.WriteLine("Third things third");
+                Console.WriteLine("Send a prayer to the ones up above");
+                Console.WriteLine("All the hate that you've heard");
+                Console.WriteLine("Has turned your spirit to a dove, oh-ooh");
+                Console.WriteLine("Your spirit up above, oh-ooh");
+                Console.WriteLine("");
+
+                Console.WriteLine("I was choking in the crowd");
+                Console.WriteLine("Building my rain up in the cloud");
+                Console.WriteLine("Falling like ashes to the ground");
+                Console.WriteLine("Hoping my feelings, they would drown");
+                Console.WriteLine("But they never did, ever lived, ebbing and flowing");
+                Console.WriteLine("Inhibited, limited");
+                Console.WriteLine("Till it broke open and rained down");
+                Console.WriteLine("And rained down, like");
+                Console.WriteLine("");
+
+                Console.ForegroundColor= ConsoleColor.DarkBlue;
+                Console.WriteLine("Pain!");
+                Console.WriteLine("You made me a, you made me a believer, believer");
+                Console.WriteLine("Pain!");
+                Console.WriteLine("You break me down and build me up, believer, believer");
+                Console.WriteLine("Pain!");
+                Console.WriteLine("Oh, let the bullets fly, oh, let them rain");
+                Console.WriteLine("My life, my love, my drive, it came from");
+                Console.WriteLine("Pain!");
+                Console.WriteLine("You made me a, you made me a believer, believer");
+                Console.ResetColor();
+                Console.WriteLine("");  
+
+                Console.WriteLine("Last things last");
+                Console.WriteLine("By the grace of the fire and the flames");
+                Console.WriteLine("You're the face of the future");
+                Console.WriteLine("The blood in my veins, oh-ooh");
+                Console.WriteLine("The blood in my veins, oh-ooh");
+                Console.WriteLine("But they never did, ever lived, ebbing and flowing");
+                Console.WriteLine("Inhibited, limited");
+                Console.WriteLine("Till it broke open and rained down");
+                Console.WriteLine("And rained down, like");
+                Console.WriteLine("");
+
+                Console.ForegroundColor= ConsoleColor.DarkBlue;
+                Console.WriteLine("Pain!");
+                Console.WriteLine("You made me a, you made me a believer, believer");
+                Console.WriteLine("Pain!");
+                Console.WriteLine("You break me down and build me up, believer, believer");
+                Console.WriteLine("Pain!");
+                Console.WriteLine("Oh, let the bullets fly, oh, let them rain");
+                Console.WriteLine("My life, my love, my drive, it came from");
+                Console.WriteLine("Pain!");
+                Console.WriteLine("You made me a, you made me a believer, believer");
+                Console.ResetColor();
+
+        }
+    }
+}
